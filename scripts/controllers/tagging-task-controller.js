b0a3d38dd4f7e3a6ac378ad188987938d83cc874
Peter Organisciak <organisciak@gmail.com>
2015-06-08 23:53:57 -0600

    Add tabbed instructions and masonry reloads

--- scripts/controllers/tagging-task-controller.js
+++ scripts/controllers/tagging-task-controller.js
@@ -1,31 +1,35 @@
 (function(){
 
 angular.module('crowdy')
- .controller("tagTaskController", ['$http', '$log', '$scope', '$routeParams', 
-         function($http, $log, $scope, $routeParams) {
+ .controller("tagTaskController", ['$http', '$log', '$scope', '$rootScope', '$routeParams', 
+         function($http, $log, $scope, $rootScope, $routeParams) {
     var tasks;
-    tasks = this;
-    tasks.preview = $routeParams.preview;
+    $scope.preview = $routeParams.preview;
     
-    this.type = "tagging";
-    tasks.items = []; // Initialize empty, set when it's available
+    $scope.type = "tagging";
+    $scope.items = []; // Initialize empty, set when it's available
 
     $scope.submitForm = function() {
         $log.log($scope);
-        $log.log($scope.turkForm.$valid);
-    };
-
-    $scope.setTags = function() {
-
-      
     };
 
     $http.get("data/taggingSample100.json").success(function(data){
-
-        tasks.items = data.slice(0,3);
-
+        $scope.items = data.slice(0,6);
     });
+
+  }])
+ .controller("tagTaskItemController", ['$http', '$log', '$scope', '$rootScope', '$routeParams', 
+         function($http, $log, $scope, $rootScope, $routeParams) {
+        // Individual Item Controller
+        $scope.$watch('tags', function(newValue, oldValue){
+          // Only refresh masonry is the value actually changed
+          // This also keeps from running on page load
+          if (newValue !== oldValue) {
+            $rootScope.$broadcast("masonry.reload");
+          }
+        });
 
   }]);
 
+// END
 })();
