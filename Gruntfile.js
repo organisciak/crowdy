fe0708bd8524ad0246fbc72c3cece7730fe3f269
Peter Organisciak <organisciak@gmail.com>
2015-07-04 14:41:45 +1000

    Remove qunit bolierplate

--- Gruntfile.js
+++ Gruntfile.js
@@ -60,17 +60,10 @@ module.exports = function (grunt) {
                 src: ['lib/**/*.js', 'test/**/*.js']
             }
         },
-        qunit: {
-            files: ['test/**/*.html']
-        },
         watch: {
             gruntfile: {
                 files: '<%= jshint.gruntfile.src %>',
                 tasks: ['jshint:gruntfile']
-            },
-            lib_test: {
-                files: '<%= jshint.lib_test.src %>',
-                tasks: ['jshint:lib_test', 'qunit']
             }
         }
     });
@@ -78,11 +71,10 @@ module.exports = function (grunt) {
     // These plugins provide necessary tasks
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Default task
-    grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+    grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
 };
 
